f6e8b80b16995645ab1140d46764eb6d5a12d69e
Lindsay M <lindsaymecher@gmail.com>
2019-11-20 03:52:59 +0000

    Done.

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,54 @@
-def square_array(array)
-  # your code here
+#def square_array(array)
+ # while numbers[0] do 
+  #  puts numbers**2
+  #  array += 1 
+#  end 
+#end
+
+#def square_array(array)
+  
+ # counter = 0 
+  #while array[0] do
+   # puts array**2 
+   # counter += 1 
+ # end 
+#end
+
+# numbers = [1, 2, 3]
+#def square_array(numbers)
+# for (let i = 0; i > 0; i++)
+#puts numbers ** 2
+# end
+# end 
+ 
+ 
+ #soft-coded?
+ 
+ numbers = [1, 2, 3]
+ def square_array(numbers)
+   
+ counter = 0
+
+while counter <= numbers.length() do
+  puts numbers[counter]**2 
+
+  counter += 1
 end
+end 
+new_numbers = square_array(numbers)
+new_numbers
+
+#hard-coded 
+
+#numbers = [1,2,3]
+#def square_array(numbers)
+
+#puts numbers[0]**2 
+#puts numbers[1]**2 
+#puts numbers[2]**2 
+#end 
+
+
+ 
+ 
+ 
