5813fd833fdb1cc0ff7835713f1c9dfbabc577f5
Lindsay M <lindsaymecher@gmail.com>
2019-12-18 00:47:38 +0000

    Automatically backed up by Learn

--- spec/square_array_spec.rb
+++ spec/square_array_spec.rb
@@ -1,19 +1,14 @@
-require_relative './spec_helper'
 
-describe '#square_array' do
+numbers = [1,2,3,4,5,6]
 
-  it 'does not call on each, collect, map, or inject' do
-    numbers = [1,2,3]
-    expect(numbers).to_not receive(:each)
-    expect(numbers).to_not receive(:collect)
-    expect(numbers).to_not receive(:map)
-    expect(numbers).to_not receive(:inject)
-    square_array(numbers)
-  end
-
-  it 'should square the elements in an array' do
-    expect(square_array([1,2,3])).to eq([1,4,9])
-    expect(square_array([9,10,16,25])).to eq([81,100,256,625])
+def square_array(numbers)
+  counter = 0
+  while counter <= numbers.length() do
+  puts numbers[counter]**2
+  counter += 1
   end
 
 end
+
+squared_numbers = square_array(numbers)
+squared_numbers
