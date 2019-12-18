bf2ca52fb71a84a5009e03359536ec029a3dabf7
Lindsay M <lindsaymecher@gmail.com>
2019-12-18 00:47:56 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,5 +2,17 @@
 
 
  
- 
+ numbers = [1,2,3,4,5,6]
+
+def square_array(numbers)
+  counter = 0
+  while counter <= numbers.length() do
+  puts numbers[counter]**2
+  counter += 1
+  end
+
+end
+
+squared_numbers = square_array(numbers)
+squared_numbers
  
